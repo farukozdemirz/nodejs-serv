2ce59ca69f6245978c47b502287c937f53729b39
farukozdemirz <faruk@weepay.co>
2022-08-22 11:42:32 +0300

    test

--- index.js
+++ index.js
@@ -7,13 +7,10 @@ const app = express();
 
 app.use(express.json());
 
-mongoose.connect(dbURL).then(() => {
-    app.listen(process.env.PORT || 5000, () => {
-        console.log('server')
-    })
-});
-
 app.get("/", (req, res) => {
     res.send("Hello world")
 })
 
+app.listen(process.env.PORT || 5000, () => {
+    console.log('server')
+})
