554db53dd3086c980888045c1d5a03d1cf77f4b0
farukozdemirz <faruk@weepay.co>
2022-08-22 11:27:36 +0300

    mongoose

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-const express = require("express");
+const mongoose = require('mongoose');
+const dbURL = 'mongodb+srv://farukozdemirz:bhathu123F@cluster0.kr1ix6u.mongodb.net/?retryWrites=true&w=majority';
 
+const express = require("express");
 const app = express();
+
+
 app.use(express.json());
+
+mongoose.connect(dbURL).then(() => {
+    app.listen(process.env.PORT || 5000, () => {
+        console.log('server')
+    })
+});
 
 app.get("/", (req, res) => {
     res.send("Hello world")
 })
 
-app.listen(process.env.PORT || 5000, () => {
-    console.log('server')
-})
