5f4dc766cae5ff279542641fedd3aa17c4997c5f
farukozdemirz <faruk@weepay.co>
2022-08-22 12:19:17 +0300

    cors added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 require('dotenv').config();
 const express = require("express");
+const cors = require('cors');
 const app = express();
+const helmet = require('helmet');
 const api = require('./api');
 
 mongoose.connect(
@@ -18,5 +20,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.send("Hello world")
 })
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
 
 app.use('/api', api);
