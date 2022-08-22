34bf7034b4c3ef70db9fcfdee7518ab7d49b9a80
farukozdemirz <faruk@weepay.co>
2022-08-22 12:05:09 +0300

    Update mongoose connection and add node version

--- index.js
+++ index.js
@@ -3,19 +3,17 @@ require('dotenv').config();
 const express = require("express");
 const app = express();
 
-mongoose
-    .connect(
-        process.env.MONGODB_URI,
-        { useNewUrlParser: true }
-    ).then((res) => console.log('connected'))
-    .catch(err => console.error('Error while connecting to DB', err));
+mongoose.connect(
+    process.env.MONGODB_URI,
+    { useNewUrlParser: true }
+).then((res) =>
+    app.listen(process.env.PORT || 5000, () => {
+        console.log('servaer')
+    }))
+    .catch(err => console.error('Error while connecting to DB'));
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Hello world")
-})
-
-app.listen(process.env.PORT || 5000, () => {
-    console.log('server')
 })
